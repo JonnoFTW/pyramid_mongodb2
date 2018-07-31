1dd675580b892f47d659213e6998bbcc489724ee
Jonathan Mackenzie <jonmac1@gmail.com>
2018-07-31 11:21:09 +0930

    Made a packaging error that made the module not work

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ testing_extras = [
 ]
 
 setup(name='pyramid_mongodb2',
-      version='1.6.1',
+      version='1.6.2',
       description='An improved package that provides mongodb connectivity. Not compatible with pyramid_mongo or pyramid_mongodb',
       long_description=README,
       long_description_content_type="text/markdown",
@@ -43,6 +43,7 @@ setup(name='pyramid_mongodb2',
       url="https://github.com/jonnoftw/pyramid_mongodb2",
       license="MIT",
       packages=find_packages('pyramid_mongodb2', exclude=['tests']),
+      package_dir={'':'.'},
       include_package_data=True,
       zip_safe=False,
       install_requires=install_requires,
